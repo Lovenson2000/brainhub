984cf51c9e5f31a7513b794118aad22d80ab8a8e
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-06-15 15:33:43 +0800

    Adding user model

--- server/main.go
+++ server/main.go
@@ -8,15 +8,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Student struct {
+type User struct {
 	ID        int    `json:"id"`
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
 	School    string `json:"school"`
 	Major     string `json:"major"`
 }
 
-var students = []Student{
+var users = []User{
 	{ID: 1, Firstname: "John", Lastname: "Doe", School: "Yuan Ze University", Major: "Computer Science"},
 	{ID: 2, Firstname: "Jane", Lastname: "Smith", School: "New York College", Major: "Biology"},
 }
@@ -27,17 +28,17 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/api/students", getStudents)
-	app.Get("/api/students/:id", getStudent)
+	app.Get("/api/users", getUsers)
+	app.Get("/api/users/:id", getUser)
 
 	log.Fatal(app.Listen(":5001"))
 }
 
-func getStudents(c *fiber.Ctx) error {
-	return c.JSON(students)
+func getUsers(c *fiber.Ctx) error {
+	return c.JSON(users)
 }
 
-func getStudent(c *fiber.Ctx) error {
+func getUser(c *fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id"))
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -45,9 +46,9 @@ func getStudent(c *fiber.Ctx) error {
 		})
 	}
 
-	for _, student := range students {
-		if student.ID == id {
-			return c.JSON(student)
+	for _, user := range users {
+		if user.ID == id {
+			return c.JSON(user)
 		}
 	}
 
