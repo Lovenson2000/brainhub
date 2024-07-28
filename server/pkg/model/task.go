8f69d478d53197e0520186f884c488af7edbb9f0
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-07-28 23:52:41 +0800

    feat: Adding Task data model

---
+++ server/pkg/model/task.go
@@ -0,0 +1,20 @@
+package model
+
+type Status string
+
+const (
+	ToDo       Status = "To Do"
+	InProgress Status = "In Progress"
+	Done       Status = "Done"
+)
+
+type Task struct {
+	ID          string `json:"id"`
+	UserID      int    `json:"user_id" db:"user_id"`
+	Title       string `json:"title" db:"title"`
+	Description string `json:"description" db:"description"`
+	StartTime   string `json:"start_time" db:"start_time"`
+	DueDate     string `json:"due_date" db:"due_date"`
+	Status      Status `json:"status" db:"status"`
+	Priority    string `json:"priority"`
+}
