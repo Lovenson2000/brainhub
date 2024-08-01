2c438c908656335acb49411bbaa3ba1442c8e634
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-08-01 14:18:56 +0800

    feat: Set up React Query for data fetching

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { ReactQueryClientProvider } from "components/ReactQueryProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ReactQueryClientProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ReactQueryClientProvider>
   );
 }
